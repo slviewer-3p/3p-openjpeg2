eae16fdf626b9201c7387e4aec5029930a70ac19
callum_linden <none@none>
2014-11-24 10:24:22 -0800

    Update files and settings for VS2013

--- build-cmd.sh
+++ build-cmd.sh
@@ -33,8 +33,8 @@ pushd "$OPENJPEG_SOURCE_DIR"
         "windows")
             load_vsvars
 
-	    cmake . -G"Visual Studio 10" -DCMAKE_INSTALL_PREFIX=$stage
-            
+            cmake . -G"Visual Studio 12" -DCMAKE_INSTALL_PREFIX=$stage
+
             build_sln "OPENJPEG.sln" "Release|Win32"
             build_sln "OPENJPEG.sln" "Debug|Win32"
             mkdir -p "$stage/lib/debug"
